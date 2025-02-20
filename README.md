a3a0b15d59a3b067283f79a42fa726e94044cb3e
YY <yy@raccoons.dev>
2025-02-20 16:06:32 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# web3-js-v22
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run index.ts
+```
+
+This project was created using `bun init` in bun v1.1.36. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
+
+Resources:
+- https://github.com/anza-xyz/solana-web3.js
+- https://station.jup.ag/docs/
+- Notes on web.js v1 to v2: https://github.com/solana-labs/solana-web3.js/issues/3498
---
+++ index.ts
@@ -0,0 +1,102 @@
+import {
+    createKeyPairSignerFromBytes,
+    createSolanaRpc,
+    createSolanaRpcSubscriptions,
+    getBase64Encoder,
+    getBase64EncodedWireTransaction,
+    getTransactionDecoder,
+} from '@solana/web3.js';
+
+import { createRecentSignatureConfirmationPromiseFactory } from '@solana/transaction-confirmation';
+
+const rpc_url = createSolanaRpc(process.env.RPC_URL || '');
+const rpc_wss = createSolanaRpcSubscriptions(process.env.RPC_WSS || '');
+
+// For Uint8Array format private key
+const privateKeyArray = JSON.parse(process.env.PRIVATE_KEY_ARRAY || '[]');
+const signer_account = await createKeyPairSignerFromBytes(new Uint8Array(privateKeyArray));
+
+// For base58 format private key
+// Import getBase58Encoder
+// const privateKeyBase58 = process.env.PRIVATE_KEY_BASE58 || '';
+// const signer_account = await createKeyPairSignerFromBytes(new Uint8Array(getBase58Encoder().encode(privateKeyBase58)));
+
+// console.log(signer_account);
+
+const quoteResponse = await (
+    await fetch(
+        'https://quote-api.jup.ag/v6/quote?inputMint=JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN&outputMint=EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v&amount=100000&slippageBps=0'
+    )
+  ).json();
+  
+// console.log(quoteResponse);
+
+const swapResponse = await (
+    await fetch('https://api.jup.ag/swap/v1/swap', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        // 'x-api-key': '',
+        },
+        body: JSON.stringify({
+            quoteResponse,
+            userPublicKey: signer_account.address,
+            priorityLevelWithMaxLamports: {
+              maxLamports: 1000,
+              priorityLevel: "veryHigh" // If you want to land transaction fast, set this to use `veryHigh`. You will pay on average higher priority fee.
+              // jitoTipLamports: 10000000
+            },
+            correctLastValidBlockHeight: true
+        })
+    })
+  ).json();
+  
+// console.log(swapResponse);
+
+const base64EncodedTransaction = swapResponse.swapTransaction;
+
+// console.log(base64EncodedTransaction);
+
+// Deserialize the base64 transaction to sign (before sending)
+// https://github.com/anza-xyz/solana-web3.js/blob/325925c76d939b2a8065f5174d99de6663255b8b/examples/deserialize-transaction/src/example.ts#L176
+const transactionBytes = getBase64Encoder().encode(base64EncodedTransaction);
+// console.log(transactionBytes);
+
+const decodedTransaction = getTransactionDecoder().decode(transactionBytes);
+// console.log(decodedTransaction);
+
+// Sign the transaction
+const [signatureDictionary] = await signer_account.signTransactions([decodedTransaction]);
+const signatureBytes = signatureDictionary[signer_account.address];
+// console.log(signatureBytes);
+
+// Now we can serialize the transaction back to base64 for sending
+const serializedTransaction = getBase64EncodedWireTransaction({
+    messageBytes: decodedTransaction.messageBytes,
+    signatures: { [signer_account.address]: signatureBytes }
+});
+// console.log(serializedTransaction);
+
+// Send the transaction
+const signature = await rpc_url.sendTransaction(serializedTransaction, { 
+    encoding: 'base64',
+    maxRetries: 0n,
+    skipPreflight: true 
+}).send();
+
+// Wait for the transaction to be confirmed
+const getRecentSignatureConfirmationPromise = createRecentSignatureConfirmationPromiseFactory({
+    rpc: rpc_url,
+    rpcSubscriptions: rpc_wss,
+});
+try {
+    await getRecentSignatureConfirmationPromise({
+        commitment: 'confirmed',
+        signature,
+        abortSignal: AbortSignal.timeout(10000),
+    });
+    console.log(`CONFIRMED: https://solscan.io/tx/${signature}`);
+} catch (e) {
+    console.error(`FAILED: https://solscan.io/tx/${signature}\n`);
+    throw e;
+}
